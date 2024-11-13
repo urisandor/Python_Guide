2636e3934cb040ea687075736ad1fb8cdc55655f
urisandor <urisandor14@gmail.com>
2024-11-13 18:12:23 +0100

    Guide

---
+++ Beginner/Typecasting.py
@@ -0,0 +1,11 @@
+
+
+name = "Sanyi" #-->  bool(name) = True, because it is NOT empty
+#name = ""      #--> bool(name) = False, because it is empty
+age = 19
+gpa = 3.2
+is_student = True
+
+#print(type(is_student))
+gpa = int(gpa)
+print(type(gpa) , gpa)
---
+++ Beginner/Variables.py
@@ -0,0 +1,42 @@
+# Variable
+
+# Strings
+first_name = "Sanyi"
+food = "pizza"
+email = "urisandor14@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+
+# Integers
+age = 19
+quantity = 3
+num_of_students = 18
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# Float
+price = 1499.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+print(f"You ran {distance}km")
+
+# Boolean
+is_student = True
+for_sale = False
+
+if is_student:
+    print(f"Are you a student?: {is_student}")
+else:
+    print("You are NOT a student")
+
+if for_sale:
+    print("That item is for sale")
+else:
+    print("That item is NOT available")
