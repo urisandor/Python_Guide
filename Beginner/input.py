17f90617eaf699ec58f643ceaca3bad8df6f3e4a
urisandor <urisandor14@gmail.com>
2024-11-13 18:40:31 +0100

    Guide

---
+++ Beginner/input.py
@@ -0,0 +1,14 @@
+#input() returns as a String
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+
+age += 1
+
+print(f"Hello {name}!")
+print(f"You are {age} years old!")
+
+
+
+
+
